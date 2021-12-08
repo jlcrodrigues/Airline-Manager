b93794f4d9e3ba7b9aa740e25a8916165c5f1932
José Luís Cunha Rodrigues <joseluiscunharodrigues@gmail.com>
2021-12-08 23:32:44 +0000

    Added tests (wip)

---
+++ src/Airline.h
@@ -0,0 +1,10 @@
+#include "Aeroport.h"
+#include "Baggage.h"
+#include "Cart.h"
+#include "Flight.h"
+#include "Passenger.h"
+#include "Plane.h"
+#include "Service.h"
+#include "Ticket.h"
+
+class Airline{};
---
+++ tests/PassengerTest.cpp
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "Airline.h"
+
+using testing::Eq;
+
+TEST(passenger, buyTicket)
+{
+   Aeroport aeroport1;
+   Aeroport aeroport2;
+   Plane plane = Plane("A310", 310);
+   Flight flight = Flight(3, "9/12/2021", "3h24", aeroport1, aeroport2, 100);
+   Passenger pa = Passenger("Carlos");
+   Passenger pa1 = Passenger("Manuel");
+   Passenger pa2 = Passenger("Maria");
+   vector<struct GroupMember> group = {{pa, true}, {pa1, false}, {pa2, true}};
+   
+   pa.buyTicket(flight);
+   EXPECT_EQ(flight.getOccupation(), 99);
+   pa.buyTicket(flight, group);
+   EXPECT_EQ(flight.getOccupation(), 96);
+
+}
