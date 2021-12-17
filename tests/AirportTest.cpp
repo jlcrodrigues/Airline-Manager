c550b9ae4341f49ef3870c690a68141826dc2dfd
andrefcbarbosa <72624261+andrefcbarbosa@users.noreply.github.com>
2021-12-17 11:22:31 +0000

    Airport tests fixed

--- tests/AirportTest.cpp
+++ tests/AirportTest.cpp
@@ -3,7 +3,7 @@
 #include "../src/Airport.h"
 
 using testing::Eq;
-/*
+
 TEST(Airport, addTransport){
    Airport a1("Sá Carneiro");
    EXPECT_EQ(a1.getName(),"Sá Carneiro");
@@ -14,13 +14,14 @@ TEST(Airport, addTransport){
    v1.push_back(d1);
 
    t1.type = "Comboio"; t2.type = "Metro"; t3.type = "Metro";
-   t1.distance = 15; t2.distance = 15 ; t3.type = 13;
+   t1.distance = 15; t2.distance = 15 ; t3.distance = 13;
    t1.time = v1; t2.time =v1; t3.time = v1;
    set<Transport> s1;
    s1.insert(t1); s1.insert(t2); s1.insert(t3);
 
    Airport a2("Humberto Delgado",s1);
-   EXPECT_EQ(a2.getTransports(),s1);
+   set<Transport>::iterator it = a2.getTransports().begin();
+   EXPECT_EQ(it->distance,13);
 
    v1.push_back(d2);
    Transport t4;
@@ -34,11 +35,21 @@ TEST(Airport, findClosest){
    vector <Date> v1;
    v1.push_back(d1);
    t1.type = "Comboio"; t2.type = "Metro"; t3.type = "Metro";
-   t1.distance = 15; t2.distance = 15 ; t3.type = 13;
+   t1.distance = 15; t2.distance = 15 ; t3.distance = 13;
    t1.time = v1; t2.time =v1; t3.time = v1;
    set<Transport> s1;
    s1.insert(t1); s1.insert(t2); s1.insert(t3);
    Airport a1("Frankfurt",s1);
    EXPECT_EQ(a1.findClosest().type,"Metro");
    EXPECT_EQ(a1.findClosest().distance,13);
-}*/
+}
+TEST(Airport, operatorOverload){
+    Transport t1, t2, t3;
+    Date d1(15,00);
+    vector <Date> v1;
+    v1.push_back(d1);
+    t1.type = "Comboio"; t2.type = "Metro"; t3.type = "Metro";
+    t1.distance = 15; t2.distance = 15 ; t3.distance = 13;
+    t1.time = v1; t2.time =v1; t3.time = v1;
+    EXPECT_TRUE(t3<t1);
+}
