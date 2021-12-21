05ce6f6a5644f9b039051a95d2ab406c26c33990
José Luís Cunha Rodrigues <joseluiscunharodrigues@gmail.com>
2021-12-21 15:30:03 +0000

    Added tutorial and about

--- src/App/App.h
+++ src/App/App.h
@@ -128,6 +128,9 @@ class App
       /**Resize the display windows.**/
       void resize();
 
+      /**Show information about the program.**/
+      void about();
+
       /**Quits the program**/
       void quit();
 
